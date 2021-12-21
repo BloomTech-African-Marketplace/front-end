cb96bdabfbf6289f02c5f090b76c6bce4802914c
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2021-12-21 17:40:36 -0600

    worked on landing

---
+++ african-marketplace/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer(){
+    return(
+    <div>
+    <h2>Contact Us!</h2>
+    <p>Email: africanmarketplace@hiowaht.com</p>
+    <p>Tel: (988) 264-4034</p>
+    
+    <p><i>Copyright 2021</i></p>
+    </div>
+    )
+}
+
+export default Footer;
---
+++ african-marketplace/src/components/LandingPage.js
@@ -0,0 +1,100 @@
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import formSchema from "./Schema";
+import * as yup from 'yup';
+import { Route,Switch } from 'react-router-dom';
+import ItemCard from "./ItemCard";
+
+
+const initialFormValues = {
+    name: '',
+    imageURL:'',
+    description:''
+}
+
+const initialFormErrors = {
+    name: '',
+    imageURL:'',
+    description:''
+}
+
+const initialItems = []
+const initialDisabled = true
+
+
+function LandingPage(){
+    const [ item,setItems ] = useState(initialItems);
+    const [ formValues,setFormValues] = useState(initialFormValues);
+    const [ formErrors,setformErrors ] = useState(initialFormErrors);
+    const [ disabled,setDisabled ] = useState(initialDisabled);
+
+    const getItems = () => {
+        axios.get('https://bwproject.herokuapp.com/api/items')
+        .then((res)=> {
+            setItems(res.data)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    }
+    const postItems = (newItem) => {
+        axios.post('https://bwproject.herokuapp.com/api/items', newItem)
+        .then(res => {
+            setItems([res.data,...item])
+        })
+        .catch(err => {
+            console.error(err)
+        })
+        .finally(()=> {
+            setFormValues(initialFormValues)
+        })
+    }
+    const validate = (name, value) => {
+        yup
+        .reach(formSchema, name)
+        .validate(value)
+        .then(()=> setformErrors({...formErrors, [name]:''}))
+        .catch((err) => setformErrors({...formErrors, [name]: err.errors[0] }))
+    }
+    const inputChange = (name, value) => {
+        validate(name, value)
+        setFormValues({
+            ...formValues,
+            [name]:value
+        })
+    }
+
+    const formSubmit = () => {
+        const newItem = {
+            name: formValues.name.trim(),
+            imageURL: formValues.imageURL.trim(),
+            description:formValues.description.trim()
+        }
+        postItems(newItem)
+    }
+    useEffect(()=> {
+        getItems()
+    }, [])
+
+    useEffect(()=> {
+        formSchema.isValid(formValues)
+        .then((valid)=> setDisabled(!valid))
+    }, [formValues])
+
+    return (
+        <Switch>
+            <Route exact path='/landing'>
+                <div>
+                    {item.map((items)=> {
+                        return <ItemCard key={items.id} details={items.id}/>
+                    })}
+                </div>
+            </Route>
+        </Switch>
+    )
+
+
+}
+
+
+export default LandingPage;
